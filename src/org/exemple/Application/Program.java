9bc98b7068d7a96e029366eadbaef1becb580c8d
Adrian-shift <20231ireinfint0049@ifba.edu.br>
2026-05-06 10:12:30 -0300

    Movendo a main para a classe Program

---
+++ src/org/exemple/Application/Program.java
@@ -0,0 +1,19 @@
+package org.exemple.Application;
+
+import org.exemple.db.DB;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Program {
+
+    public static void main(String[] args){
+
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+    }
+
+}
